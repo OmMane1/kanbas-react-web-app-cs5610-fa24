cdf21cffe6f39a2422aea7514fdd8f4b290eedb2
MiyuqiKazumei <76604271+MiyuqiKazumei@users.noreply.github.com>
2024-09-09 12:19:55 -0400

    getting started on a1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Web App.
+          Welcome to WEB APP
         </p>
         <a
           className="App-link"
