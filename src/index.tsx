eeacdf7328a724c854abf11ca240511f3f91280a
OmMane1 <ommane1@gmail.com>
2024-10-15 23:11:34 -0400

    lab 3

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import './index.css';
+
 
 
 const root = ReactDOM.createRoot(
