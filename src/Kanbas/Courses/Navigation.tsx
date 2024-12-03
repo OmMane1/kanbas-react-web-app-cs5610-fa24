ea7ececd49292856fad7d7656b7890c112e2ec95
OmMane1 <ommane1@gmail.com>
2024-12-03 11:34:11 -0500

    Course Navigation UI fixed

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link, useParams, useLocation } from 'react-router-dom';
-import '../styles.css';
+import '../styles.css'; // Ensure this file contains your custom styles.
 
 export default function CoursesNavigation() {
   const { cid } = useParams();
   const location = useLocation();
-   
+
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
 
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => {
         const linkPath = `/Kanbas/Courses/${cid}/${link}`;
-        const isActive = location.pathname.endsWith(link);
+        const isActive = location.pathname.includes(linkPath);
 
         return (
           <Link
             key={link}
             id={`wd-course-${link.toLowerCase()}-link`}
-            className={`list-group-item ${isActive ? 'active' : ''}`}
+            className={`list-group-item link-width ${isActive ? 'active' : 'text-danger'} border border-0`}
             to={linkPath}
           >
             {link}
