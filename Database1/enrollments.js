cbee41c2948207086bc0a13bd5b4f88d865895ec
OmMane1 <ommane1@gmail.com>
2024-12-04 17:22:46 -0500

    database update

---
+++ Database1/enrollments.js
@@ -0,0 +1,18 @@
+[
+    {  "user": "ObjectId('6750d1ae6441519e15f85642')", "course": "RS101" },
+    {  "user": "ObjectId('6750d1ae6441519e15f85642')", "course": "RS102" },
+
+    {  "user": "ObjectId('6750d1ae6441519e15f85643')", "course": "RS101" },
+    {  "user": "ObjectId('6750d1ae6441519e15f85643')", "course": "RS102" },
+
+    {  "user": "ObjectId('6750d1ae6441519e15f85644')", "course": "RS101" },
+
+    { "user": "ObjectId('6750d1ae6441519e15f85645')", "course": "RS101" },
+
+    {  "user": "ObjectId('6750d1ae6441519e15f85646')", "course": "RS101" },
+
+    {  "user": "ObjectId('6750d1ae6441519e15f85647')", "course": "RS102" },
+    
+    {  "user": "ObjectId('6750d1ae6441519e15f85648')", "course": "RS102" }
+    
+  ]
